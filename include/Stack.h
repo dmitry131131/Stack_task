93ce1bbf89e8e3beb0c15bead992a8937f12858f
dmitry131131 <antontsev.dmitry@yandex.ru>
2023-09-21 01:15:24 +0300

    Created Stack structure and verify function

--- include/Stack.h
+++ include/Stack.h
@@ -4,7 +4,24 @@
 */
 #ifndef STACK_H
 #define STACK_H
+typedef int elem_t;
 
+struct Stack
+{
+    elem_t* data;
+    size_t  size;
+    size_t  capacity;
+};
+
+enum errorCode
+{
+    NO_ERRORS,
+    NO_STACK_PTR,
+    NO_STACK_DATA_PTR,
+    SIZE_OUT_OF_CAPACITY,
+    SIZE_NOT_VALID,
+    CAPACITY_NOT_VALID
+};
 
 
 #endif
--- src/Stack.cpp
+++ src/Stack.cpp
@@ -2,3 +2,15 @@
  * @file
  * @brief Struct functions source
 */
+#include "Stack.h"
+
+enum errorCode stack_verify(struct Stack* stack)
+{
+    if (!stack)                         return NO_STACK_PTR;
+    if (!stack->data)                   return NO_STACK_DATA_PTR;
+    if (stack->size >= stack->capacity) return SIZE_OUT_OF_CAPACITY;
+    if (stack->size     == -1)          return SIZE_NOT_VALID;
+    if (stack->capacity == -1)          return CAPACITY_NOT_VALID;
+
+    return NO_ERRORS;
+}
