3821c1416bc07e801286002e5478132fbd04b453
dmitry131131 <antontsev.dmitry@yandex.ru>
2023-09-21 12:13:13 +0300

    Made pull, push, ctor and dtor

--- include/Stack.h
+++ include/Stack.h
@@ -5,23 +5,70 @@
 #ifndef STACK_H
 #define STACK_H
 typedef int elem_t;
+const elem_t ELEM_T_POISON = 0;
 
+const size_t REALLOC_COEF        =  2;
+const size_t SIZE_POISON_VAL     = -1;
+const size_t CAPACITY_POISON_VAL = -1;
+
+/// @brief Stack struct
 struct Stack
 {
-    elem_t* data;
-    size_t  size;
-    size_t  capacity;
+    elem_t* data;       ///< Data array pointer
+    size_t  size;       ///< Stack size(position of last element in array)
+    size_t  capacity;   ///< Capacity of stack(max length of stack array)
 };
 
+/// @brief enum with stack error codes
 enum errorCode
 {
-    NO_ERRORS,
-    NO_STACK_PTR,
-    NO_STACK_DATA_PTR,
-    SIZE_OUT_OF_CAPACITY,
-    SIZE_NOT_VALID,
-    CAPACITY_NOT_VALID
+    NO_ERRORS,              ///< No errors(everything is Ok)
+    NO_STACK_PTR,           ///< No stack pointer(passed NULL pointer to stack struct)
+    NO_STACK_DATA_PTR,      ///< No data pointer(passed NULL pointer to stack data)
+    SIZE_OUT_OF_CAPACITY,   ///< Size more than capacity(size >= capacity)
+    SIZE_NOT_VALID,         ///< Size variable filled poison value 
+    CAPACITY_NOT_VALID,     ///< Capacity variable filled poison value
+    NO_MEMORY,              ///< Calloc function can't alloc the memory
+    EMPTY_STACK             ///< Size is zero(empty stack)
 };
 
+#define CHECK_ERR(code)do{        \
+    enum errorCode error = code;  \
+    if (error)                    \
+    {                             \
+        return error;             \
+    }                             \
+}while(0)
+
+
+/**
+ * @brief Verification function for stack - check all stack data is valid
+ * @param [in] stack Pointer to stack
+ * @return Error code or NO_ERRORS if everything ok
+*/
+enum errorCode stack_verify(struct Stack* stack);
+
+/**
+ * @brief Function initializes stack
+ * @param [out] stack     Pointer to stack
+ * @param [in]  capacity  Start capacity of stack  
+ * @return Error code or NO_ERRORS if everything ok
+*/
+enum errorCode stack_ctor(struct Stack* stack, size_t capacity);
+
+/**
+ * @brief Function destruct stack
+ * @param [in] stack Pointer to stack
+ * @return Error code or NO_ERRORS if everything ok
+*/
+enum errorCode stack_dtor(struct Stack* stack);
+
+/**
+ * @brief Function puts value into stack
+ * @param [in] stack Pointer to stack
+ * @param [in] value Value to push
+ * @return Error code and NO_ERRORS if everythind ok
+*/
+enum errorCode stack_push(struct Stack* stack, elem_t value);
 
 #endif
--- src/Stack.cpp
+++ src/Stack.cpp
@@ -2,15 +2,91 @@
  * @file
  * @brief Struct functions source
 */
+#include <stdio.h>
+#include <stdlib.h>
+
 #include "Stack.h"
 
 enum errorCode stack_verify(struct Stack* stack)
 {
-    if (!stack)                         return NO_STACK_PTR;
-    if (!stack->data)                   return NO_STACK_DATA_PTR;
-    if (stack->size >= stack->capacity) return SIZE_OUT_OF_CAPACITY;
-    if (stack->size     == -1)          return SIZE_NOT_VALID;
-    if (stack->capacity == -1)          return CAPACITY_NOT_VALID;
+    if (!stack)                                     return NO_STACK_PTR;
+    if (!stack->data)                               return NO_STACK_DATA_PTR;
+    if (stack->size >= stack->capacity)             return SIZE_OUT_OF_CAPACITY;
+    if (stack->size     == SIZE_POISON_VAL)         return SIZE_NOT_VALID;
+    if (stack->capacity == CAPACITY_POISON_VAL)     return CAPACITY_NOT_VALID;
 
     return NO_ERRORS;
+}
+
+enum errorCode stack_ctor(struct Stack* stack, size_t capacity)
+{
+    if (!stack)                             return NO_STACK_PTR;
+    if (capacity == CAPACITY_POISON_VAL)    return CAPACITY_NOT_VALID;
+
+    stack->data = (elem_t*) calloc(capacity, sizeof(elem_t));
+    if (!stack->data) return NO_MEMORY;
+
+    stack->capacity = capacity;
+    stack->size     = 0;
+
+    return NO_ERRORS;
+}
+
+enum errorCode stack_dtor(struct Stack* stack)
+{
+    if (!stack)         return NO_STACK_PTR;
+    if (!stack->data)   return NO_STACK_DATA_PTR;
+
+    free(stack->data);
+    stack->data     = NULL;
+    stack->size     = SIZE_POISON_VAL;
+    stack->capacity = CAPACITY_POISON_VAL;
+
+    return NO_ERRORS;
+}
+
+enum errorCode stack_realloc(struct Stack* stack)
+{
+    if (!stack) return NO_STACK_PTR;
+
+    if (stack->size + 1 == stack->capacity)
+    {
+        stack->data      = (elem_t*) realloc(stack->data, stack->capacity * sizeof(elem_t) * REALLOC_COEF);
+        stack->capacity *= REALLOC_COEF;
+    }
+    if (stack->size <= (size_t) stack->capacity / 4)
+    {
+        stack->data      = (elem_t*) realloc(stack->data, (size_t) stack->capacity * sizeof(elem_t) / REALLOC_COEF);
+        stack->capacity /= REALLOC_COEF;
+    }
+
+    if (!stack->data) return NO_MEMORY;
+
+    return NO_ERRORS;
+}
+
+enum errorCode stack_push(struct Stack* stack, elem_t value)
+{
+    if (!stack) return NO_STACK_PTR;
+
+    CHECK_ERR(stack_realloc(stack));
+
+    stack->data[stack->size++] = value;
+
+    return NO_ERRORS;
+}
+
+elem_t stack_pull(struct Stack* stack)
+{
+    if (!stack) return NO_STACK_PTR;
+
+    CHECK_ERR(stack_realloc(stack));
+
+    if (stack->size == 0) return EMPTY_STACK;
+    stack->size--;
+
+    elem_t ret = stack->data[stack->size];
+    stack->data[stack->size] = ELEM_T_POISON;
+
+    return ret;
 }
